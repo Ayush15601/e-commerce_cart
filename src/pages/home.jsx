f3dc7324e9c9b5b4232cad2384de2750804f9b86
Ayush15601 <aayushtewarit@gmail.com>
2026-06-20 23:08:23 +0530

    feat: created search box for products

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -10,6 +10,8 @@ function Home() {
     const [product, setproduct] = useState([])
     const [err, seterr] = useState(null)
     const [loading, setloading] = useState(false)
+    const [query, setquery] = useState("")
+    const [submited, setsubmited] = useState("")
 
     useEffect( () => {
 
@@ -33,23 +35,54 @@ function Home() {
 
         loadproducts()
     }, [])
-  
+
+    const load = (e) => {
+
+        e.preventDefault()
+
+        if(loading) return
+
+        setsubmited(query)
+    }
+
+    const change = (e) => {
+
+        const product = e.target.value
+        setquery(product)
+
+        if(product === ""){
+            setsubmited("")
+        }
+    }
+
+    const filteredProducts = product.filter((item) => item.title.toLowerCase().includes(submited.toLowerCase()))
+    
     return (
         
         <>
         
             <Navbar />  
+                
+            <form className="h_form" onSubmit={load}>
+
+                <input type="text" onChange={change} value={query} placeholder="Enter Your Product Name" name="search box"/>
+                
+                <input type="submit" value="Search" disabled={!query.trim()}/>
+            
+            </form>
 
            <div>
 
                 {err && <div className="h_error"> {err} </div>}
             
-                {loading ? (<div className="h_loading"> <p> loading poducts... </p> </div>) : (<div className="h_box"> {product.map((item) => (<Box product={item} key={item.id}/>))} </div>)}
+                {/* {loading ? (<div className="h_loading"> <p> loading poducts... </p> </div>) : (<div className="h_box"> {product.map((item) => (<Box product={item} key={item.id}/>))} </div>)} */}
+
+                {loading ? ( <div className="h-loading"> loading products... </div> ) : (<div className="h_box"> {filteredProducts.map((item) => ( <Box product={item} key={item.id}/> ))} </div> )}
          
            </div>
         
         </>
-  )
+    )
 
 }
 
